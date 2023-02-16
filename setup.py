c85957538e0d36934b5cbc22e7e8d9a9d2cef990
Jackson Dean <jacksonsdean@gmail.com>
2023-02-16 10:39:56 -0500

    change package name

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='cppn-pytorch',
+    name='cppn_pytorch',
     version='0.0.1',
     author='Jackson Dean',
     author_email='jackson@downbeat.games',
     description='CPPN implementation in PyTorch',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/jacksonsdean/cppn-pytorch',
+    url='https://github.com/jacksonsdean/cppn_pytorch',
     project_urls = {
-        "Bug Tracker": "https://github.com/jacksonsdean/cppn-pytorch/issues"
+        "Bug Tracker": "https://github.com/jacksonsdean/cppn_pytorch/issues"
     },
     license='MIT',
-    packages=['cppn-pytorch'],
+    packages=['cppn_pytorch'],
     install_requires=['torch', 'numpy', 'matplotlib', 'scipy', 'scikit-image', 'functorch', 'tqdm', 'torchvision', 'piq', 'imageio','scikits-bootstrap'],
 )
