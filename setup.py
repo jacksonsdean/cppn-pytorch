9d1ea4cf3af7c2df060f2c93884744a83e0c09fa
Jackson Dean <jacksonsdean@gmail.com>
2023-02-22 14:39:25 -0500

    change to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='cppn_torch',
-    version='0.0.15',
-    author='Jackson Dean',
-    author_email='jackson@downbeat.games',
-    description='CPPN implementation in PyTorch',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/jacksonsdean/cppn_torch',
-    project_urls = {
-        "Bug Tracker": "https://github.com/jacksonsdean/cppn_torch/issues"
-    },
-    license='MIT',
-    packages=['cppn_torch'],
-    install_requires=['torch', 'numpy', 'matplotlib', 'scipy', 'scikit-image', 'functorch', 'tqdm', 'torchvision', 'piq', 'imageio','scikits-bootstrap', 'networkx'],
-)
+if __name__ == "__main__":
+    setup()
